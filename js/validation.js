9be678d5b23bf20abac31d386ab1489058543be4
Ravikant Pal <ravikant.pal@kane-group.com>
2020-09-05 18:25:56 +0530

    V1.2.0

--- js/validation.js
+++ js/validation.js
@@ -32,6 +32,32 @@ $(function () {
     console.log(flag+"  <=========");
     return flag;
   }
+
+  function isIncrOrDescNum(str,val) {
+    let flag=false;
+    if(val.match(/\d+/g)==null) {
+      console.log("null h re baba!")
+    } else {
+      val.match(/\d+/g).forEach((val)=> {
+        flag = flag || (LCSubStr(str,val,str.length,val.length)>2);
+      });
+    }
+    console.log(flag+"  <=========");
+    return flag;
+  }
+
+  function isIncrOrDescAlpha(str,val) {
+    let flag=false;
+    if(val.match(/[a-zA-Z]+/g)==null) {
+      console.log("null h re baba!")
+    } else {
+      val.match(/[a-zA-Z]+/g).forEach((val)=> {
+        flag = flag || (LCSubStr(str,val,str.length,val.length)>2);
+      });
+    }
+    console.log(flag+"  <=========");
+    return flag;
+  }
   // Adding the custom validation
 
   //For Increasing Sequence
@@ -40,7 +66,7 @@ $(function () {
       function (value, element) {
         return !isIncrOrDescNum("0123456789",value);
       },
-      `<p class="text-danger small">opps! Increasing Sequence not allowed!</p>`
+      `<p class="text-danger small">opps! Increasing Number Sequence not allowed!</p>`
   );
 
   //For Decreasing Sequence
@@ -49,17 +75,66 @@ $(function () {
       function (value, element) {
         return !isIncrOrDescNum("9876543210",value);
       },
-      `<p class="text-danger small">opps! Decreasing Sequence not allowed!</p>`
+      `<p class="text-danger small">opps! Decreasing Number Sequence not allowed!</p>`
   );
 
-  //For valid repeated number
+  $.validator.addMethod(
+      "incrChar",
+      function (value, element) {
+        return !isIncrOrDescAlpha("abcdefghijklmnopqrstuvwxyz",value);
+      },
+      `<p class="text-danger small">opps! Increasing Alphabet Sequence not allowed!</p>`
+  );
+
+  $.validator.addMethod(
+      "descChar",
+      function (value, element) {
+        return !isIncrOrDescAlpha("zyxwvutsrqponmlkjihgfedcba",value);
+      },
+      `<p class="text-danger small">opps! Decreasing Alphabet Sequence not allowed!</p>`
+  );
+  //For valid repeated character more than 3 character
   $.validator.addMethod(
       "repNumber",
       function (value, element) {
-        return !value.match("([\\d])\\1{2,}");
+        return !value.match("(.)\\1{2,}");
       },
-      `<p class="text-danger small">Please do not put repeated number!</p>`
+      `<p class="text-danger small">Please do not put repeated character!</p>`
   );
+
+  $.validator.addMethod(
+      "AtLeastOneLAC",
+      function (value, element) {
+        return value.match("(?=.*[a-z])");
+      },
+      `<p class="text-danger small">Password should contain at least 1 lowercase alphabetical character!</p>`
+  );
+
+  $.validator.addMethod(
+      "AtLeastOneUAC",
+      function (value, element) {
+        return value.match("(?=.*[A-Z])");
+      },
+      `<p class="text-danger small">Password should contain at least 1 uppercase alphabetical character!</p>`
+  );
+
+  $.validator.addMethod(
+      "AtLeastOneNC",
+      function (value, element) {
+        return value.match("(?=.*[0-9])");
+      },
+      `<p class="text-danger small">Password should contain at least 1 numeric character!</p>`
+  );
+
+  $.validator.addMethod(
+      "AtLeastOneSC",
+      function (value, element) {
+        return value.match("(?=.*[!@#$%^&*])");
+      },
+      `<p class="text-danger small">Password should contain at least 1 special character!</p>`
+  );
+
+
 
   $("#user-input").on("submit", function (event) {
     // adding rules for inputs with class 'comment'
@@ -69,6 +144,12 @@ $(function () {
         repNumber: true,
         incrNum:true,
         descNum:true,
+        incrChar:true,
+        descChar:true,
+        AtLeastOneLAC:true,
+        AtLeastOneUAC:true,
+        AtLeastOneNC:true,
+        AtLeastOneSC:true,
         minlength: 8,
         messages: {
           required: `<p class="text-danger small">opps! required</p>`,
